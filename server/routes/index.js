01bd01dccba4f9574b95b65500767760afed6fc0
Manuel Muñoz Solera <mamuso@mamuso.net>
2019-05-19 23:40:31 -0700

    require db

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,11 @@
 'use strict';
 
 const express = require('express');
+const db = require('../db');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
+router.get('/', async (req, res, next) => {
   res.json({ test: 'test' });
 });
 
