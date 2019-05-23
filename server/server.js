7f71f937b94414d66ee9ccf1e4e69807f8ec9019
Manuel Muñoz Solera <mamuso@mamuso.net>
2019-05-22 21:37:29 -0700

    graphql

--- server/server.js
+++ server/server.js
@@ -2,22 +2,46 @@
 
 require('dotenv').config();
 const express = require('express');
-const session = require('express-session');
-const apiRouter = require('./routes');
+const graphqlHTTP = require('express-graphql');
+const { buildSchema } = require('graphql');
+// const session = require('express-session');
+// const apiRouter = require('./routes');
 
 const app = express();
 
-app.use(express.json());
+// app.use(express.json());
 
-app.use('/v1', apiRouter);
+// app.use('/v1', apiRouter);
+
+// app.use(
+//   session({
+//     secret: process.env.SESSION_SECRET,
+//     resave: true,
+//     saveUninitialized: true
+//   })
+// );
+
+// Construct a schema, using GraphQL schema language
+var MyGraphQLSchema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+var root = {
+  hello: () => {
+    return 'Hello world!';
+  },
+};
 
 app.use(
-  session({
-    secret: process.env.SESSION_SECRET,
-    resave: true,
-    saveUninitialized: true
-  })
-);
+  '/graphql',
+  graphqlHTTP({
+    schema: MyGraphQLSchema,
+    rootValue: root,
+    graphiql: true
+}));
 
 app.listen(process.env.PORT || 3000, () => {
   console.log(`Server is running on port ${process.env.PORT || 3000}`);
